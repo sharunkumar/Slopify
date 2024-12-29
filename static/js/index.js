7851047e680512f0bc318bc1696ef01c7779cf93
Svapnil <svapnila@gmail.com>
2024-12-29 11:23:30 -0500

    added css/javascript for dancing frog

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,7 @@
 function vibe() {
     document.getElementById('vibeAudio').play();
+
+    document.getElementById('frog').id = 'dancing-frog';
 }
 
 document.getElementById('vibeButton').addEventListener('click', vibe);
