8d7323c5912950e33a62c15960d3d3146f0b0fda
Kyle <kyle@kyles1872.com>
2024-12-29 18:31:51 +0000

    hello rick

--- static/js/index.js
+++ static/js/index.js
@@ -1,15 +1,30 @@
+const frog = document.getElementById('frog');
+const moreVibeAudio = document.getElementById('moreVibeAudio');
+const vibeAudio = document.getElementById('vibeAudio');
+
 function vibe() {
-    document.getElementById('moreVibeAudio').pause(); document.getElementById('moreVibeAudio').currentTime = 0; 
-    document.getElementById('vibeAudio').pause(); document.getElementById('vibeAudio').currentTime = 0;
+    moreVibeAudio.pause(); moreVibeAudio.currentTime = 0;
+    vibeAudio.pause(); vibeAudio.currentTime = 0;
     const asd = Math.round(Math.random() * 690);
     console.log(asd)
-    if (String(asd).includes("69") || String(asd).includes("42"))
-        document.getElementById('moreVibeAudio').play(); 
-    else 
-    document.getElementById('vibeAudio').play();
+    if (String(asd).includes("69") || String(asd).includes("42")) {
+        frog.src = 'static/images/rick.gif';
+        moreVibeAudio.play();
+    } else {
+        vibeAudio.play();
+    }
 
-    document.getElementById('frog').id = 'dancing-frog';
+    frog.id = 'dancing-frog';
 }
+
+// no more dancing and no more rick
+vibeAudio.addEventListener('ended', () => frog.id = 'frog');
+moreVibeAudio.addEventListener('ended', function() {
+    frog.id = 'frog';
+    frog.src = 'static/images/frog.webp';
+});
+moreVibeAudio.addEventListener('ended', () => frog.src = 'static/images/frog.webp');
+moreVibeAudio.addEventListener('pause', () => frog.src = 'static/images/frog.webp');
 
 // TODO: Refactor random number generation
 document.body.style.background = 'hsl('+Math.floor(Math.random() * 255)+', 40%, 50%)';
