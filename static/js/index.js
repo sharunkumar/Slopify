991e95113846edfede2573c0c847e30c8734723e
出口レイ <richard@kanshen.click>
2024-12-29 17:58:24 +0100

    did the stupidest thing ever imaginable
    i am so sorry

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,13 @@
 function vibe() {
+    document.getElementById('moreVibeAudio').pause(); document.getElementById('moreVibeAudio').currentTime = 0; 
+    document.getElementById('vibeAudio').pause(); document.getElementById('vibeAudio').currentTime = 0;
+    const asd = Math.round(Math.random() * 690);
+    console.log(asd)
+    if (String(asd).includes("69") || String(asd).includes("42"))
+        document.getElementById('moreVibeAudio').play(); 
+    else 
     document.getElementById('vibeAudio').play();
 }
 
 document.getElementById('vibeButton').addEventListener('click', vibe);
+
