802eb9452e1a8b5192d8ff4aed9ae581240e80e6
Zebulun McNeill <zeb.mcn@gmail.com>
2024-12-31 13:10:36 -0600

    basic structure 🍆

---
+++ Slopify/src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div className="container">
+      <h1>Welcome to Slopify</h1>
+      <p>This is the home page of the official Slopify application.</p>
+    </div>
+  );
+}
+
