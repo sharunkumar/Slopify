7a4fe76bd91975477e6dffb1782c5dad50dba50c
LordDeatHunter <desuhansa@gmail.com>
2025-01-04 16:20:54 +0100

    Clean up some code

--- static/js/comicButton.js
+++ static/js/comicButton.js
@@ -1,11 +1,15 @@
-var isComic = false;
-var comicButton = document.getElementById("comicButton");
-comicButton.addEventListener("click", () => {
-  if (!isComic) {
-    document.body.setAttribute("id", "comic-font");
-    isComic = true;
-  } else {
-    document.body.removeAttribute("id");
-    isComic = false;
-  }
+let isComic = false;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const comicButton = document.getElementById("comicButton");
+
+  comicButton.addEventListener("click", () => {
+    if (!isComic) {
+      document.body.setAttribute("id", "comic-font");
+      isComic = true;
+    } else {
+      document.body.removeAttribute("id");
+      isComic = false;
+    }
+  });
 });
