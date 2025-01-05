b4b1719c7db0d8e1d49006c178bd5e68f641883e
Zebulun McNeill <zeb.mcn@gmail.com>
2025-01-04 18:49:38 -0600

    fix aud

--- Slopify/src/App.jsx
+++ Slopify/src/App.jsx
@@ -4,8 +4,39 @@ import ChatPage from "./pages/ChatPage";
 import SlopboxPage from "./pages/Slopbox";
 import ProfilePage from "./pages/ProfilePage";
 import "./styles/global.css";
+import { check } from "@tauri-apps/plugin-updater";
+import { relaunch } from "@tauri-apps/plugin-process";
+
+const update = await check();
+  if (update) {
+    console.log(`found udpate ${update.version} from ${update.date} with notes ${update.body}`);
+    let downloaded = 0;
+    let contentLength = 0;
+  
+    await update.downloadAndInstall((event) => {
+      switch (event.event) {
+        case "Started":
+          contentLength = event.data.contentLength;
+          console.log(`started downloading ${event.data.contentLength} bytes`);
+          break;
+        case "Progress":
+          downloaded += event.data.chunkLength;
+          console.log(`downloaded ${downloaded} from ${contentLength}`);
+          break;
+        case "Finished":
+          console.log("download finished");
+          break;
+      }
+    });
+  
+    console.log("update installed");
+    await relaunch();
+  } else {
+    console.log("no updates available")
+  }
 
 export default function App() {
+
   return (
     <div className="app" style={{ backgroundColor: "var(--bg-dark)" }}>
       <main>
